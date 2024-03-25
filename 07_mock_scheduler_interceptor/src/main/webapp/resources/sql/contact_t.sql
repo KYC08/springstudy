d0752cb8ff91ae6c9436dd8c8ef11a6a909377e4
KYC08 <dkswoah127@naver.com>
2024-03-25 17:24:43 +0900

    20240325

--- 07_mock_scheduler_interceptor/src/main/webapp/resources/sql/contact_t.sql
+++ 07_mock_scheduler_interceptor/src/main/webapp/resources/sql/contact_t.sql
@@ -1,13 +1,17 @@
-DROP TABLE CONTACT_T;
-CREATE TABLE CONTACT_T (
-  CONTACT_NO NUMBER             NOT NULL,
-  NAME       VARCHAR2(100 BYTE) NOT NULL,
-  MOBILE     VARCHAR2(20 BYTE),
-  EMAIL      VARCHAR2(100 BYTE),
-  ADDRESS    VARCHAR2(200 BYTE),
-  CREATED_AT VARCHAR2(20 BYTE),
-  CONSTRAINT PK_CONTACT PRIMARY KEY(CONTACT_NO)
-);
+ES8에서 도입된 async와 await
 
-DROP SEQUENCE CONTACT_SEQ;
-CREATE SEQUENCE CONTACT_SEQ NOCACHE;
+async 키워드
+1. 비동기 함수를 만드는 키워드
+2. 함수 앞에 추가
+async function함수이름(){}
+async function(){}
+async ()=>{}
+const 함수이름 = async function(){}
+const 함수이름 = async ()=>{}
+async 함수는 항상 Promise를 반환한다.
+
+await 키워드
+1. Promise 이행을 기다리기 위해서 사용한다.
+2. 오직 async 함수 내부에서만 사용할 수 있다.
+3. 형식
+value = await expression
---
+++ 08_file/src/main/java/com/gdu/prj08/controller/MvcController.java
@@ -0,0 +1,18 @@
+package com.gdu.prj08.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class MvcController {
+  
+  @GetMapping(value={"/", "/main.do"})
+  public String welcome() {
+    log.info("you can confirm value = {}", StructuredArguments.value("key", "value"));
+    return "index";
+  }
+
+}
