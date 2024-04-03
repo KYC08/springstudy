1bf566988b969c975253fa6624de80e18144bf07
KYC08 <dkswoah127@naver.com>
2024-04-03 16:56:48 +0900

    20240403

--- myapp/src/main/java/com/gdu/myapp/mapper/UserMapper.java
+++ myapp/src/main/java/com/gdu/myapp/mapper/UserMapper.java
@@ -2,9 +2,12 @@ package com.gdu.myapp.mapper;
 
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.gdu.myapp.dto.LeaveUserDto;
 import com.gdu.myapp.dto.UserDto;
 
+@Mapper
 public interface UserMapper {
   UserDto getUserByMap(Map<String, Object> map);
   LeaveUserDto getLeaveUserByMap(Map<String, Object> map);
